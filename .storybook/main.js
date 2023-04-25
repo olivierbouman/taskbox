2357d0871b7f13cb9fa3b5968dc6472e35bbb7a7
Olivier Bouman <obouman@deloitte.nl>
2023-04-25 14:38:29 +0200

    init again

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,23 @@
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/components/**/*.stories.js"],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
     "@storybook/preset-create-react-app",
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y",
+    "@storybook/addon-mdx-gfm",
   ],
-  framework: "@storybook/react",
-  core: {
-    builder: "@storybook/builder-webpack5",
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {},
   },
   features: {
     interactionsDebugger: true,
+  },
+  docs: {
+    autodocs: true,
   },
 };
